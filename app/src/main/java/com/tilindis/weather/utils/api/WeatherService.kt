010140d7d69fa559cc47321af33866ab3b9c3e0f
Tilindis <heronightmares@gmail.com>
2023-03-08 08:44:41 +0200

    Update data retrieve logic

--- app/src/main/java/com/tilindis/weather/utils/api/WeatherService.kt
+++ app/src/main/java/com/tilindis/weather/utils/api/WeatherService.kt
@@ -4,16 +4,12 @@ import com.tilindis.weather.utils.response.WeatherResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WeatherService {
-    // veikia
-    // org @GET("forecast?latitude=54.689159&longitude=25.2798&timezone=auto&current_weather=true&hourly=temperature_2m,winddirection_10m")
-    @GET("forecast?latitude={latitude}&longitude={longitude}&timezone=auto&current_weather=true&hourly=temperature_2m,winddirection_10m")
+    @GET("v1/forecast?timezone=auto&current_weather=true&hourly=temperature_2m,winddirection_10m")
     suspend fun getWeather(
-        @Path("latitude", encoded = true) latitude: String,
-        @Path("longitude", encoded = true) longitude: String
+        @Query("latitude") lat: String,
+        @Query("longitude") long: String
     ): Response<WeatherResponse>
-
-    // metodas su tusciais parametrais veikia
-    //suspend fun getWeather(): Response<WeatherResponse>
 }
--- app/src/main/java/com/tilindis/weather/utils/other/Constants.kt
+++ app/src/main/java/com/tilindis/weather/utils/other/Constants.kt
@@ -2,6 +2,6 @@ package com.tilindis.weather.utils.other
 
 object Constants {
     const val DATASTORE_NAME = "DataStore"
-    const val WEATHER_URL = "https://api.open-meteo.com/v1/"
+    const val WEATHER_URL = "https://api.open-meteo.com/"
     const val RETROFIT_WEATHER_NAME = "RETROFIT_WEATHER"
 }
